0579d1e06becfe6fcd1c9f3d1bec5714ff53debd
DcPepper <genest.thomas.ba@gmail.com>
2023-06-24 23:20:18 +0200

    Ajout des musiques et images

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("chat/", include("chat.urls")),
+    path("", include("chat.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
